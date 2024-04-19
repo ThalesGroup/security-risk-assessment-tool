f9f4b1672942447be6d12190fea278801211d3f3
ThomasDThales <164328304+ThomasDThales@users.noreply.github.com>
2024-04-19 14:52:33 +0800

    fix panel switch on vulnerability

--- app/src/tabs/Vulnerabilities/renderer.js
+++ app/src/tabs/Vulnerabilities/renderer.js
@@ -322,12 +322,11 @@
         vulnerabilitiesData = data.Vulnerability;
         supportingAssetsData = data.SupportingAsset;
         if (vulnerabilitiesData.length === 0) $('#vulnerabilities section').hide();
-        else $('#vulnerabilities section').show();
-        
-        updateSupportingAssets(data.SupportingAsset);
-        
-
-        updateVulnerabilityFields(vulnerabilitiesData);
+        else{
+            $('#vulnerabilities section').show();  
+            updateSupportingAssets(data.SupportingAsset);
+            updateVulnerabilityFields(vulnerabilitiesData);
+        }
     };
 
     $(document).ready(async function () {
