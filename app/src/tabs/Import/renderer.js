8cc44079b46ec4637ab62689ccc77bbc38662622
borkthecat <readersfiction@gmail.com>
2026-07-01 15:05:31 +0800

    cleanups

--- app/src/tabs/Import/renderer.js
+++ app/src/tabs/Import/renderer.js
@@ -82,9 +82,6 @@ const getSeverityColor = (level) => {
       columns: [ 
         {
           title: 'Import?', field: 'checkbox', headerSort: false, headerHozAlign: 'center', hozAlign: 'center', headerWordWrap: true, width: 80
-        },
-        {
-          title: 'Supporting Asset Name', field: 'supportingAssetName', headerSort: false, headerHozAlign: 'center', hozAlign: 'center'
         }
       ],
   }
@@ -112,9 +109,6 @@ const getSeverityColor = (level) => {
         },
         {
           title: 'Risk Level', field: 'residualRiskLevel', headerSort: false, headerHozAlign: 'center', hozAlign: 'center', width: 80
-        },
-        {
-          title: 'Mgnt Decision', field: 'riskManagementDecision', headerSort: false, headerHozAlign: 'center', hozAlign: 'center', width: 140
         }
       ],
   }
